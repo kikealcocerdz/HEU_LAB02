8783ed3a61e3e23f5a1fdc693f1d79cc0864dc69
100472175 <100472175@alumnos.uc3m.es>
2023-12-02 22:27:38 +0100

    Prueba de restricciones

--- p2-472175-472118/parte1/CSPParking.py
+++ p2-472175-472118/parte1/CSPParking.py
@@ -8,31 +8,71 @@ with open(ruta_parking, 'r') as archivo:
     lineas = archivo.readlines()
     # quiero no guardarme los /n
     lineas = [linea.strip() for linea in lineas]
-    coches = len(lineas) - 2
+    # Toma los coches que hay que meter en el parking
+    coches = lineas[2:]
+
+    # Número de coches para meter en el parking
+    num_coches = len(coches) 
     matriz = lineas[0].split("x")
-    filas = int(matriz[0])
-    columnas = int(matriz[1])
+    filas = int(matriz[0]) # Número de filas del parking
+    columnas = int(matriz[1]) # Número de columnas del parking
+
+    cargadores = lineas[1][3:].split(") (")
+    cargadores[0] = cargadores[0][1:]
+    cargadores[-1] = cargadores[-1][:-1]
+    cargadores = [cargador.replace(",", "") for cargador in cargadores] # Posiciones de los cargadores
+
+
 
 # Un parking tiene sus aparcamientos numerados por filas y columnas, la columna 7 es la salida del garaje
 # Los vehiculos que aparcan en esto aparcamientos pueden ser de dos tipos, TSU y TNU
-# En las plazas (1,1), (1,2), (2,1), (4,1), (5,1), (5,2) hay conexión eléctrica
 problem = Problem()
 
-letras = []
-letra = "a"
-for i in range(coches):
-    # Código a ejecutar en cada iteración del bucle
-    letra = chr(ord(letra) + 1)
-    letras.append(letra)
+# Crea una matriz de todas las plazas del parking disponibles, como elementos vacios de una lista de listas
+plazas = [[str(i)+str(j) for j in range(1, columnas + 1)] for i in range(1, filas + 1)]
+plazas = [plaza for linea in plazas for plaza in linea]
 
-for i in range(coches):
-  problem.addVariable(letras[i], [range(1, filas), range(1,columnas)])
-  coches -= 1
+# Crea el dominio
+print("Coches: ", coches, "\n")
+print("Plazas: ", plazas, "\n")
+print("Cargadores: ", cargadores, "\n")
+
+problem.addVariables(coches, plazas)
+
+# Crea las restricciones
+# Cada coche tiene que tener una plaza distinta
+problem.addConstraint(AllDifferentConstraint(), coches)
+
+# Cada coche solo puede tener una plaza
+# problem.addConstraint(ExactSumConstraint(1), coches)
+
+# Cada coche con congelador, tiene que tener una conexión eléctrica en su plaza
+print("Coches con congelador: ", [coche for coche in coches if coche[-1] == "C"])
+for coche in coches:
+    if coche[-1] == "C":
+        problem.addConstraint(lambda plaza: plaza in cargadores, [coche])
 
 
-# Restricciones
-# Todos los vehículos tienen asignada una sola plaza
+# Vehículos de tipo TSU no pueden tener delante un vehículo de tipo TNU. 
+# Vamos, que la segunda coordenada de la plaza del TSU tiene que ser menor que la del TNU
+for i in range(0, len(coches)):
+    for j in range(i + 1, len(coches)):
+        if coches[i][3] == "T" and coches[j][3] == "N":
+            problem.addConstraint(lambda plaza1, plaza2: int(plaza1[1]) < int(plaza2[1]), [coches[i], coches[j]])
 
+
+# Por cuestiones de maniobrabilidad dentro del parking todo vehículo debe tener libre una plaza a izquierda
+# o derecha (mirando en direcci´on a la salida). Por ejemplo, si un veh´ıculo ocupa la plaza 3.3 no podrá tener
+# aparcado un vehículo en la 2.3 y otro en la 4.3, al menos una de esas dos plazas deber´a quedar libre.
+for i in range(0, len(coches)):
+    for j in range(i + 1, len(coches)):
+        problem.addConstraint(lambda plaza1, plaza2: int(abs(int(plaza1[0]) - int(plaza2[0]))) >= 2, [coches[i], coches[j]])
+
+solutions = problem.getSolutions()
+print("Solutions: ", solutions, "\n")
+
+
+exit()
 nombre_archivo = str(ruta_parking)
 
 # Generar el nombre del archivo de salida
