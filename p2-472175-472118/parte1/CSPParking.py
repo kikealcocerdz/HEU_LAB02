1a152d4373b84fcd2f0a091195daa87836ae7504
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-12-15 01:28:41 +0100

    tests parte 1

--- p2-472175-472118/parte1/CSPParking.py
+++ p2-472175-472118/parte1/CSPParking.py
@@ -1,8 +1,43 @@
 from constraint import *
 from pprint import pprint
+import pandas as pd
 import time
+import random
 import sys
 import os
+import pprint
+
+
+def escribe_sol(solucion):
+    parking = {}
+    for i in range(1, filas + 1):
+        for j in range(1, columnas + 1):
+            parking[(i, j)] = "-"
+    solution_cars = [[value[0], value[1], key] for key, value in solucion.items()]
+
+    for car in solution_cars:
+        row, col, car_id = car
+        parking[(row, col)] = car_id
+
+    resultado = ""
+    comas_ocupado = ","
+    comas_libre = ","
+    try:
+        if sys.argv[2] == "bonito":
+            comas_ocupado = "\t,\t"
+            comas_libre = "\t\t\t,\t"
+    except IndexError:
+        pass
+    for i in range(1, filas + 1):
+        for j in range(1, columnas + 1):
+            if len(parking[(i, j)]) > 4:  # Si hay coche
+                addition = f"\"{parking[(i, j)]}\"{comas_ocupado}"
+            else:
+                addition = f"\"{parking[(i, j)]}\"{comas_libre}"
+            resultado += addition
+        resultado += "\n"
+    return resultado
+
 
 ruta_parking = sys.argv[1]
 
@@ -14,33 +49,23 @@ with open(ruta_parking, 'r') as archivo:
     coches = lineas[2:]
 
     # Número de coches para meter en el parking
-    num_coches = len(coches) 
+    num_coches = len(coches)
     matriz = lineas[0].split("x")
-    filas = int(matriz[0]) # Número de filas del parking
-    columnas = int(matriz[1]) # Número de columnas del parking
+    global filas, columnas
+    filas = int(matriz[0])  # Número de filas del parking
+    columnas = int(matriz[1])  # Número de columnas del parking
 
-    cargadores = lineas[1][3:].split(") (")
-    cargadores[0] = cargadores[0][1:]
-    cargadores[-1] = cargadores[-1][:-1]
-    cargadores = [cargador.replace(",", "") for cargador in cargadores] # Posiciones de los cargadores
-
-
+    cargadores = lineas[1].replace("PE:", "").replace(")", ")p").split("p")[:-1]
+    cargadores = [(int(cargador[2]), int(cargador[-2])) for cargador in cargadores]
 
 # Un parking tiene sus aparcamientos numerados por filas y columnas, la columna 7 es la salida del garaje
 # Los vehiculos que aparcan en esto aparcamientos pueden ser de dos tipos, TSU y TNU
 problem = Problem()
 
 # Crea una matriz de todas las plazas del parking disponibles, como elementos vacios de una lista de listas
-plazas = [[str(i)+str(j) for j in range(1, columnas + 1)] for i in range(1, filas + 1)]
-plazas = [plaza for linea in plazas for plaza in linea]
-
-# Crea el dominio
-print("Coches: ", coches, "\n")
-print("Plazas: ", plazas, "\n")
-print("Cargadores: ", cargadores, "\n")
-
+plazas = [(i, j) for j in range(1, columnas + 1) for i in range(1, filas + 1)]
+# Crea el dominio, asigna a cada coche todas las plazas posibles.
 problem.addVariables(coches, plazas)
-
 
 # Crea las restricciones
 # La primera restricción es el comportamiento por defecto en python-constraints
@@ -48,94 +73,68 @@ problem.addVariables(coches, plazas)
 # 2. Cada coche tiene que tener una plaza distinta
 problem.addConstraint(AllDifferentConstraint())
 
-
-
 # 3. Cada coche con congelador, tiene que tener una conexión eléctrica en su plaza
-print("Coches con congelador: ", [coche for coche in coches if coche[-1] == "C"])
-for coche in coches:
-    if coche[-1] == "C":
-        problem.addConstraint(lambda plaza: plaza in cargadores, [coche])
+# Toma solo los coches que tienen un congelador e itera por la lista
+for coche in [coche for coche in coches if coche[-1] == "C"]:
+    problem.addConstraint(lambda plaza: plaza in cargadores, [coche])
 
 
-# 4. Vehículos de tipo TSU no pueden tener delante un vehículo de tipo TNU. 
+# 4. Vehículos de tipo TSU no pueden tener delante un vehículo de tipo TNU.
 # Vamos, que la segunda coordenada de la plaza del TSU tiene que ser menor que la del TNU
-for i in range(0, len(coches)):
-    for j in range(0, len(coches)):
-        if coches[i][3] == "S" and coches[j][3] == "N":
-            problem.addConstraint(lambda plaza1, plaza2:
-                                   int(plaza1[1]) > int(plaza2[1]) if (plaza1[0]==plaza2[0]) else True,
-                                     [coches[i], coches[j]])
+def comprueba1(plaza1: tuple, plaza2: tuple) -> bool:
+    if plaza1[0] == plaza2[0]:
+        if plaza1[1] < plaza2[1]:
+            return False
+    return True
 
 
 # 5. Por cuestiones de maniobrabilidad dentro del parking todo vehículo debe tener libre una plaza a izquierda
 # o derecha (mirando en direcci´on a la salida). Por ejemplo, si un veh´ıculo ocupa la plaza 3.3 no podrá tener
 # aparcado un vehículo en la 2.3 y otro en la 4.3, al menos una de esas dos plazas deberá quedar libre.
 
-def comprueba(plaza1: str, plaza2: str, plaza3: str) -> bool:
-    if plaza1[0] == 1 or plaza1[0] == filas:
-        if abs(int(plaza1[0]) - int(plaza2[0])) == 1 or abs(int(plaza1[0]) - int(plaza3[0])) == 1:
-            return False
-        
-    if abs(int(plaza1[0]) - int(plaza2[0])) == 1 and abs(int(plaza1[0]) - int(plaza3[0])) == 1:
-        if (plaza1[1] == plaza2[1]) and (plaza1[1] == plaza3[1]) and (plaza2[1] == plaza3[1]):
-            return False
-
-    return True
-    
-        
-    """if plaza1[1] != plaza2[1] or plaza1[1] != plaza3[1]:
-        return True
-    
+def comprueba2(plaza1: tuple, plaza2: tuple) -> bool:
     if int(plaza1[0]) == 1 or int(plaza1[0]) == filas:
-        if abs(int(plaza2[0]) - int(plaza1[0])) == 1 or abs(int(plaza3[0]) - int(plaza1[0])) == 1:
-            return False
-
-    
-    
-    if abs(int(plaza1[0]) - int(plaza2[0])) == 1 and abs(int(plaza1[0]) - int(plaza3[0])) == 1:
-        return False
-
+        if abs(int(plaza1[0]) - int(plaza2[0])) == 1:
+            if plaza1[1] == plaza2[1]:
+                return False
     return True
 
-    if plaza1[1] != plaza2[1] :
-        return True
-    if int(plaza1[0]) == 1 or int(plaza1[0]) == filas:
-        if abs(int(plaza2[0]) - int(plaza1[0])) == 1:
-            return False
-        if abs(int(plaza3[0]) - int(plaza1[0])) == 1:
-            return False
 
+def comprueba3(plaza1: tuple, plaza2: tuple, plaza3: tuple) -> bool:
     if abs(int(plaza1[0]) - int(plaza2[0])) == 1 and abs(int(plaza1[0]) - int(plaza3[0])) == 1:
-        return False
-    
-    return True"""
+        if (plaza1[1] == plaza2[1]) and (plaza2[1] == plaza3[1]):
+            return False
+    return True
 
 
+for i in range(len(coches)):
+    for j in range(len(coches)):
+        if i != j:
+            if "S" in coches[i] and "N" in coches[j]:
+                problem.addConstraint(comprueba1, [coches[i], coches[j]])
+            problem.addConstraint(comprueba2, [coches[i], coches[j]])
+            for k in range(len(coches)):
+                if i < k:
+                    problem.addConstraint(comprueba3, [coches[i], coches[j], coches[k]])
 
-for i in range(0, len(coches)):
-    for j in range(0, len(coches)):
-        for k in range(0, len(coches)):
-            if i != j and i != k and j != k:
-                problem.addConstraint(comprueba, [coches[i], coches[j], coches[k]])
-
-a = time.time()
-# Resuelve el problema
-print("Resuelve el problema")
+# Salida del programa
 solutions = problem.getSolutions()
-#pprint(solutions)
-b = time.time()
-print("Tiempo de ejecución: ", b - a, "\n")
-print("Soluciones: ", solutions.__len__(), "\n")
-exit()
-
-
-nombre_archivo = str(ruta_parking)
+solutions_dictionary = [{v: k for k, v in solutions.items()} for solutions in solutions]
 
 # Generar el nombre del archivo de salida
+nombre_archivo = str(ruta_parking)
 nombre_salida = os.path.splitext(nombre_archivo)[0] + ".csv"
 
 # Escribir los datos en el archivo de salida
 with open(nombre_salida, "w") as archivo:
-    # Escribir el número de soluciones encontradas
-    archivo.write("N. Sol:" + problem.getSolutions().__len__().__str__() + "\n")
-    
+    archivo.write("N. Sol:" + solutions.__len__().__str__() + "\n")
+    # Escribir las soluciones encontradas
+    if solutions.__len__() < 10:
+        solutions_indexes = range(0, solutions.__len__())
+    else:
+        solutions_indexes = [random.randint(0, solutions.__len__()) for _ in range(10)]
+    for solution in solutions_indexes:
+        archivo.write(escribe_sol(solutions[solution]))
+        archivo.write("\n")
+
+print("Soluciones: ", solutions.__len__(), "\n")
